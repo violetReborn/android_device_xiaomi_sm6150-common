38c78ae3fe9693b46660c087086352b20ff7da1b
GoZiko <anirban89m@gmail.com>
2025-04-25 19:32:51 +0530

    Update sm6150.mk Welcome Pixelage

--- sm6150.mk
+++ sm6150.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020-2025 The GenesisOS Project
+# Copyright (C) 2020-2025 The PixelageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -205,13 +205,13 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Genesis Health
+# Lineage Health
 PRODUCT_PACKAGES += \
-    vendor.genesis.health-service.default
+    vendor.lineage.health-service.default
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.genesis.livedisplay@2.1-service.xiaomi_sm6150
+    vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -227,7 +227,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-genesis
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
@@ -241,7 +241,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.genesis-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
 # QTI
@@ -280,7 +280,7 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/genesis/interfaces/power-libperfmgr \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
